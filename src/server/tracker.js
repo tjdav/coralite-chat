a3ad650451dc0500849143071eb900f66c3870b2
Thomas David <tom@mightysmile.com>
2026-03-26 22:22:46 +0100

    chore: clean up tracker.js comments and formatting

--- src/server/tracker.js
+++ src/server/tracker.js
@@ -3,26 +3,22 @@ import { Server } from 'bittorrent-tracker'
 const port = process.env.TRACKER_PORT || 8000
 
 const server = new Server({
-  udp: false, // only enable ws (webrtc tracker)
+  udp: false,
   http: false,
   ws: true,
-  stats: true // update tracker stats
+  stats: true
 })
 
 server.on('error', function (err) {
-  // fatal server error!
   console.log(err.message)
 })
 
 server.on('warning', function (err) {
-  // client sent bad data. probably not a problem, just a buggy client.
   console.log(err.message)
 })
 
 server.on('listening', function () {
-  // fired when all requested servers are listening
   console.log('WebRTC Private Tracker listening on ws port ' + port)
 })
 
-// start tracker server listening! Use 0.0.0.0 to listen on all interfaces
 server.listen(port, '0.0.0.0')
